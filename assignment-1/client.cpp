973433801e99ef7921e119d98350d487cf1ce336
Rasmus <91119694+Rasweb@users.noreply.github.com>
2025-05-13 17:11:50 +0200

    Working connection
    * Working connection using server and client.
    * Server listens on loopback address.
    * Message is sent by client and recievd by server

--- assignment-1/client.cpp
+++ assignment-1/client.cpp
@@ -4,48 +4,93 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#include <string>
+#include <vector>
+#define SERVER_PORT 8080
+#define IP_TO_LISTEN_TO "127.0.0.1"
+#define DATA_TO_SEND "NORMAL_DATA:Hello"
+#define SOCKET_CONNECT_MSG "Socket connect msg"
+#define SOCKET_SEND_MSG "Socket send msg"
+
+void errorCheck(int var, char *msg)
+{
+    if (var < 0)
+    {
+        std::cerr << msg << ": " << errno << std::endl;
+    }
+}
+
 int main()
 {
-    const char *msg_after = "wakwaka";
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    int clientSocket;
+    int socketConnectReturn;
+    int socketSendReturn;
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    std::string message_to_send = DATA_TO_SEND;
+
+    sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(SERVER_PORT);
+
+    if (inet_pton(AF_INET, IP_TO_LISTEN_TO, &serverAddress.sin_addr) <= 0)
     {
-        std::cerr << "Socket creation error" << std::endl;
-        return 1;
+        std::cerr << "Invalid address or not supported" << std::endl;
+        close(clientSocket);
+        return 0;
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
+    socketConnectReturn = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    errorCheck(socketConnectReturn, SOCKET_CONNECT_MSG);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <=
-        0)
-    {
-        std::cerr << "Invalid address or address not supported" << std::endl;
-        close(sock);
-        return 1;
-    }
+    socketSendReturn = send(clientSocket, message_to_send.c_str(), message_to_send.length(), 0);
+    errorCheck(socketSendReturn, SOCKET_SEND_MSG);
 
-    std::cout << "Connecting to server..." << std::endl;
-    if (connect(sock, (struct sockaddr *)&server_addr,
-                sizeof(server_addr)) < 0)
-    {
-        std::cerr << "Connection failed" << std::endl;
-        close(sock);
-        return 1;
-    }
+    std::cout << "Sent to registry server: " << message_to_send << std::endl;
 
-    char buffer[BUFFER_SIZE] = {0};
-    int valread = read(sock, buffer, BUFFER_SIZE);
+    close(clientSocket);
 
-    std::cout << "Message from server: " << buffer << std::endl;
+    // int clientFd;
+    // struct sockaddr_in registryServerAddr;
+    // int registryServerInet;
+    // int socketConnect;
+    // std::string my_server_ip = MY_SERVER_IP_STRING;
+    // int my_server_port = STUDENT_SERVER_PORT;
+    // std::string message_to_send = my_server_ip + ":" + std::to_string(my_server_port);
+    // int socketSend;
+    // char reciveBuffer[1024];
 
-    send(sock, msg_after, strlen(msg_after), 0);
-    std::cout << "Connection message sent" << std::endl;
+    // clientFd = socket(AF_INET, SOCK_STREAM, 0);
+    // if (clientFd < 0)
+    // {
+    //     std::cerr << "Socket creation error" << std::endl;
+    // }
+    // memset(&registryServerAddr, 0, sizeof(registryServerAddr));
+    // registryServerAddr.sin_family = AF_INET;
+    // registryServerAddr.sin_port = htons(INSTRUCTOR_REGISTRY_PORT);
+    // registryServerInet = inet_pton(AF_INET, INSTRUCTOR_REGISTRY_IP, &registryServerAddr.sin_addr);
+    // if (registryServerInet == 0)
+    // {
+    //     std::cerr << "Registry server ip conversion failed" << std::endl;
+    // }
 
-    close(sock);
+    // socketConnect = connect(clientFd, (struct sockaddr *)&registryServerAddr, sizeof(registryServerAddr));
+
+    // if (socketConnect < 0)
+    // {
+    //     std::cerr << "Connection failed" << std::endl;
+    //     close(clientFd);
+    // }
+
+    // socketSend = send(clientFd, message_to_send.c_str(), message_to_send.length(), 0);
+    // if (socketSend == -1)
+    // {
+    //     std::cerr << "Failed to send" << std::endl;
+    // }
+    // std::cout << "Sent to Registry Server: " << message_to_send << std::endl;
+    // recv(clientFd, &reciveBuffer, sizeof(reciveBuffer) - 1, 0);
+    // close(clientFd);
+
     return 0;
 }
--- assignment-1/server.cpp
+++ assignment-1/server.cpp
@@ -3,74 +3,132 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#include <arpa/inet.h>
+#define SERVER_PORT 8080
+#define REQUEST_BACKLOG 5
+#define IP_TO_LISTEN_TO "127.0.0.1"
+#define SOCKET_CREATE_MSG "Socket creation error"
+#define SOCKET_OPT_MSG "Socket options error"
+#define SOCKET_BIND_MSG "Socket bind error"
+#define SOCKET_LISTEN_MSG "Socket listen error"
+#define SOCKET_ACCEPT_MSG "Socket accept error"
+#define SOCKET_RECEIVE_MSG "Socket receive error"
+
+void errorCheck(int var, char *msg)
+{
+    if (var < 0)
+    {
+        std::cerr << msg << ": " << errno << std::endl;
+    }
+}
+
 int main()
 {
-    const char *welcome_message = "Hello from IoT Network Programming Server !";
+    int serverSocketFd;
+    int socketOptReturn;
+    int socketBindReturn;
+    int socketListenReturn;
 
-    int server_fd;
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        std::cerr << "Socket creation failed" << std::endl;
-        return 1;
-    }
+    struct sockaddr_in serverAdress;
+
+    serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+    errorCheck(serverSocketFd, SOCKET_CREATE_MSG);
 
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt,
-                   sizeof(opt)))
+
+    int socketOptReturn = setsockopt(serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    errorCheck(socketOptReturn, SOCKET_OPT_MSG);
+
+    memset(&serverAdress, 0, sizeof(serverAdress));
+    serverAdress.sin_family = AF_INET;
+    serverAdress.sin_addr.s_addr = inet_addr(IP_TO_LISTEN_TO);
+    serverAdress.sin_port = htons(SERVER_PORT);
+
+    socketBindReturn = bind(serverSocketFd, (struct sockaddr *)&serverAdress, sizeof(serverAdress));
+    errorCheck(socketBindReturn, SOCKET_BIND_MSG);
+
+    socketListenReturn = listen(serverSocketFd, 5);
+    errorCheck(socketListenReturn, SOCKET_LISTEN_MSG);
+
+    while (1)
     {
-        std::cerr << "setsockopt failed" << std::endl;
-        close(server_fd);
-        return 1;
-    }
+        int clientSocket = accept(serverSocketFd, NULL, NULL);
+        errorCheck(clientSocket, SOCKET_ACCEPT_MSG);
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+        char buffer[1024];
+        int reciveBytes = recv(clientSocket, buffer, sizeof(buffer), 0);
+        errorCheck(reciveBytes, SOCKET_RECEIVE_MSG);
 
-    if (bind(server_fd, (struct sockaddr *)&address,
-             sizeof(address)) < 0)
-    {
-        std::cerr << "Bind failed" << std::endl;
-        close(server_fd);
-        return 1;
-    }
-
-    if (listen(server_fd, 3) < 0)
-    { // Allow up to 3 pending connections
-        std::cerr << "Listen failed" << std::endl;
-        close(server_fd);
-        return 1;
-    }
-
-    std::cout << "Server listening on port " << PORT << "..." << std::endl;
-
-    while (true)
-    {
-        struct sockaddr_in client_addr;
-        socklen_t addrlen = sizeof(client_addr);
-        int client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addrlen);
-        if (client_socket < 0)
+        if (reciveBytes > 0)
         {
-            std::cerr << "Accept failed" << std::endl;
-            continue;
+            buffer[reciveBytes] = '\0';
+            std::cout << "Received from client: " << buffer << std::endl;
         }
 
-        std::cout << "New client connected" << std::endl;
-
-        send(client_socket, welcome_message,
-             strlen(welcome_message), 0);
-        std::cout << "Welcome message sent" << std::endl;
-
-        char buffer[BUFFER_SIZE] = {0};
-        int valread = read(client_socket, buffer, BUFFER_SIZE);
-        std::cout << "Connection msg from client: " << buffer << std::endl;
-
-        close(client_socket);
+        close(serverSocketFd);
     }
+    // int serverFd;
+    // int socketOptions;
+    // struct sockaddr_in serverAddr;
+    // int socketBind;
+    // int socketListen;
+    // char instructorBuffer[1024];
 
-    close(server_fd);
+    // serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    // if (serverFd < 0)
+    // {
+    //     std::cerr << "Socket creation error" << std::endl;
+    // }
+
+    // int optval = 1;
+    // socketOptions = setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &optval,
+    //                            sizeof(optval));
+    // if (socketOptions < 0)
+    // {
+    //     std::cerr << "Failed to set socket options" << std::endl;
+    // }
+
+    // memset(&serverAddr, 0, sizeof(serverAddr));
+    // serverAddr.sin_family = AF_INET;
+    // serverAddr.sin_addr.s_addr = INADDR_ANY;
+    // serverAddr.sin_port = htons(STUDENT_SERVER_PORT);
+
+    // socketBind = bind(serverFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    // if (socketBind < 0)
+    // {
+    //     std::cerr << "Socket binding failed" << std::endl;
+    //     close(serverFd);
+    // }
+
+    // socketListen = listen(serverFd, REQUEST_BACKLOG);
+    // if (socketListen < 0)
+    // {
+    //     std::cerr << "Socket listen failed" << std::endl;
+    //     close(serverFd);
+    // }
+
+    // std::cout << "Student server listening on port " << STUDENT_SERVER_PORT << "..." << std::endl;
+
+    // while (1)
+    // {
+    //     struct sockaddr_in instructor_addr;
+    //     socklen_t instructor_addr_len = sizeof(instructor_addr);
+    //     int instructor_conn_fd = accept(serverFd, (struct sockaddr *)&instructor_addr, &instructor_addr_len);
+    //     if (instructor_conn_fd < 0)
+    //     {
+    //         std::cerr << "Accept failed" << std::endl;
+    //         close(instructor_conn_fd);
+    //     }
+
+    //     char instructor_ip_str[INET_ADDRSTRLEN];
+    //     inet_ntop(AF_INET, &instructor_addr.sin_addr, instructor_ip_str, INET_ADDRSTRLEN);
+    //     std::cout << "Connection accepted from instructor's server at " << instructor_ip_str << ":" << ntohs(instructor_addr.sin_port) << std::endl;
+    //     recv(instructor_conn_fd, &instructorBuffer, sizeof(instructorBuffer) - 1, 0);
+    //     close(instructor_conn_fd);
+    //     close(serverFd);
+
+    //     /* code */
+    // }
+
     return 0;
 }
